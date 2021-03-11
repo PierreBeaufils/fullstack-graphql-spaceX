9df291ef8cbf9408f6247b19675d1d0a1cbb46d2
Pierre Laptop <pierre.beaufils@outlook.fr>
2021-03-11 19:10:58 +0100

    set up schema

--- app/server/src/index.js
+++ app/server/src/index.js
@@ -1,1 +1,10 @@
 require('dotenv').config();
+
+const {
+    ApolloServer
+} = require('apollo-server');
+const typeDefs = require('./schema.graphql');
+
+const server = new ApolloServer({
+    typeDefs
+});
