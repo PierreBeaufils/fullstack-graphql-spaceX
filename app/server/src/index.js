0d8a503a5e7213b0451be87811785ed324a423e2
Pierre Laptop <pierre.beaufils@outlook.fr>
2021-03-13 15:45:00 +0100

    launchAPI class

--- app/server/src/index.js
+++ app/server/src/index.js
@@ -3,8 +3,16 @@ require('dotenv').config();
 const {
     ApolloServer
 } = require('apollo-server');
-const typeDefs = require('./schema.graphql');
+const typeDefs = require('./schema');
 
 const server = new ApolloServer({
     typeDefs
+});
+
+server.listen().then(() => {
+    console.log(`
+      Server is running!
+      Listening on port 4000
+      Explore at https://studio.apollographql.com/dev
+    `);
 });
--- app/server/src/schema.js
+++ app/server/src/schema.js
@@ -3,6 +3,12 @@ const {
 } = require('apollo-server');
 
 const typeDefs = gql `
+    type Query {
+        launches: [Launch]
+        launch(id: ID!): Launch
+        me: User
+    }
+
     type Launch {
         id: ID!
         site: String
@@ -16,22 +22,34 @@ const typeDefs = gql `
         name: String
         type: String
     }
-
-    type Mission {
-        name: String
-        missionPatch(size: PatchSize): String
-    }
-
+      
     type User {
         id: ID!
         email: String!
         trips: [Launch]!
         token: String
     }
-
+      
+    type Mission {
+        name: String
+        missionPatch(size: PatchSize): String
+    }
+      
     enum PatchSize {
         SMALL
         LARGE
+    }
+
+    type Mutation {
+        bookTrips(launchIds: [ID]!): TripUpdateResponse!
+        cancelTrip(launchId: ID!): TripUpdateResponse!
+        login(email: String): User
+    }
+
+    type TripUpdateResponse {
+        success: Boolean!
+        message: String
+        launches: [Launch]
     }
 `;
 
